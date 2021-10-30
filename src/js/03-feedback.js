beb382e460506741b135b09d69a34309a5c9cda5
AnastasiiaBrd <nastuxa1357@gmail.com>
2021-10-31 00:29:43 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+initForm();
+form.addEventListener(`input`, throttle(onInputForm, 500));
 
+function onInputForm() {
+  const formData = new FormData(form);
+  let userForm = {};
+  formData.forEach((value, name) => (userForm[name] = value));
+  localStorage.setItem(localStorageKey, JSON.stringify(userForm));
+}
+function initForm() {
+  let persistedForm = localStorage.getItem(localStorageKey);
+  if (persistedForm) {
+    persistedForm = JSON.parse(persistedForm);
+    Object.entries(persistedForm).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
+form.addEventListener(`submit`, onSubmitForm);
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  let userForm = {};
+  const formData = new FormData(form);
+  formData.forEach((value, name) => (userForm[name] = value));
+  console.log(userForm);
+  form.reset();
+}
